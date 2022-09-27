2d0f3da89d8524a7905c4ce444bf9766414df294
ypsilone <wtf@gmail.com>
2022-09-27 19:45:24 +0500

    Добавлен посетитель для проверки на нечетность

--- src/main/java/org/hse/course/tasks/service/Visitor.java
+++ src/main/java/org/hse/course/tasks/service/Visitor.java
@@ -8,7 +8,7 @@ import org.hse.course.tasks.data.Ticket;
  * @param <R> тип результата работы метода {@link Visitor#visit(Object)}
  */
 public interface Visitor<T, R> {
-    //todo добавить посетителя, проверяющего кратность пяти + фабрику таких посетитиелей
+
     static Factory<Ticket, Boolean> getEvenVisitorFactory() {
         return new EvenTicketVisitorFactory();
     }
@@ -33,6 +33,23 @@ class EvenTicketVisitorFactory implements Visitor.Factory<Ticket, Boolean> {
         public Boolean visit(Ticket item) {
             var modulo = item.getNumber() % 2;
             return modulo == 0;
+        }
+    }
+}
+
+class OddTicketVisitorFactory implements Visitor.Factory<Ticket, Boolean> {
+
+    @Override
+    public Visitor<Ticket, Boolean> getInstance() {
+        return new OddTicketVisitorFactory.OddTicketVisitorImpl();
+    }
+
+    private static class OddTicketVisitorImpl implements Visitor<Ticket, Boolean> {
+
+        @Override
+        public Boolean visit(Ticket item) {
+            var modulo = item.getNumber() % 2;
+            return modulo == 1;
         }
     }
 }
